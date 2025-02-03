a7315d64ffa59580053494c57e8899ac613e9fda
Mohammad Hamdan <mohammadasdjangodev@gmail.com>
2025-02-03 23:53:10 +0300

    Refactor class-based views
    Adding some comments to make my code more readable, and fix some bugs

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
 from django.core.cache import cache
-from django.views.decorators.http import require_GET
-from core.utils import paginate
 from django.views import generic
 from django.db.models import Q
 
@@ -27,33 +24,41 @@ def search_bar_context():
     return context
 
 class ProjectsListView(generic.ListView):
+    """View to display a paginated list of projects."""
     paginate_by = 5
     model = Project
     template_name = "projects/project_page.html"
     context_object_name = "projects"
 
     def get_queryset(self):
+        """Retrieve non-deleted projects ordered by creation date."""
         return super().get_queryset()\
-            .filter(deleted_at=None)\
+            .filter(Q(deleted_at__isnull=True))\
                 .order_by('-created_at')\
                     .prefetch_related('tags')
     
     def get_context_data(self, **kwargs):
+        """Add the search bar context to the template."""
         context = super().get_context_data(**kwargs)
         context["search_bar"] = search_bar_context()
         return context
     
 
 class ProjectsSearchView(generic.ListView):
+    """View to display a filtered list of projects based on search criteria."""
     context_object_name = "projects"
     paginate_by = 5
     template_name = "projects/project_page.html"
     model = Project
 
     def get_queryset(self):
+        """Filter projects based on title and tags."""
         title_query = self.request.GET.get("title", "")
         tags_query = self.request.GET.getlist("tags" , [])
-        tags = [int(tag) for tag in tags_query]
+        try:
+            tags = [int(tag) for tag in tags_query]
+        except ValueError:
+            tags = []   # Ignore invalid tag values
 
         title_queryObj = Q(
             deleted_at = None,
@@ -66,20 +71,25 @@ class ProjectsSearchView(generic.ListView):
 
         projects_queryset =  super().get_queryset()
 
+        # Apply filters and return sorted results
         return projects_queryset.filter(
             title_queryObj & tags_queryObj
         ).distinct().order_by("-created_at").prefetch_related("tags")
 
-    def get_context_data(self, **kwargs) -> dict[str]:
+    def get_context_data(self, **kwargs):
+        """Add search parameters and search bar context to the template."""
         context = super().get_context_data(**kwargs)
 
+        # Build query string for pagination & filters
         title_query = self.request.GET.get("title")
         tags_queries = self.request.GET.getlist("tags" , [])
-        tags_str = "&".join(list(map(lambda tag : f"tags={tag}", tags_queries)))
+        query_params = []
 
-        query_str = f"title={title_query}&{tags_str}"
+        if title_query:
+            query_params.append(f"title={title_query}")
+        query_params.extend(f"tags={tag}" for tag in tags_queries)
 
+        context["query_string"] = "&".join(query_params)
         context["search_bar"] = search_bar_context()
-        context["query_string"] = query_str
         return context
     
