22c97a307d8d3abc164923a544860f082ab0855e
Mohammad <mohammad@local>
2026-07-16 23:28:13 +0300

    add render to the allowed hosts.

--- portfolio/settings/prod.py
+++ portfolio/settings/prod.py
@@ -2,7 +2,12 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', 'mohammadasdjangodev.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'localhost',
+    'mohammadasdjangodev.pythonanywhere.com',
+    'django-portfolio-yuuf.onrender.com',
+    '.onrender.com',
+]
 
 # Production cache and DB already in base
 print("🏭 Running in PRODUCTION mode")
